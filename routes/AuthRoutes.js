22d208b9b3031d2da35db1cd0a433b3dfe35d551
Arinze <arinze19@outlook.com>
2021-06-09 11:10:08 +0100

    remove test route

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -4,7 +4,6 @@ class AuthRoutes {
   static route(router) {
     router.route('/sign-up').post(AuthCtrl.signUp);
     router.route('/sign-in').post(AuthCtrl.signIn);
-    router.route('/stuff').get((req, res)  => res.send({ data: req.headers }));
   }
 }
 
